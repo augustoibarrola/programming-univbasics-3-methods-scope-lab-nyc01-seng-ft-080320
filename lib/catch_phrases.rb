be5734969031df123e14412fc1280fd645fdcf03
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-16 18:04:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,14 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  link_says = 'It\'s Dangerous To Go Alone! Take This.'
+  puts link_says
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
